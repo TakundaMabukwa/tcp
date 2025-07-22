3d74f1391de1a7c379c971465f2a6dac2f67e45d
TakundaMabukwa <mabukwatakunda@gmail.com>
2025-07-22 22:44:42 +0200

    ...

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
-const {createClient} = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config(); // If using dotenv
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = "https://aaidndkmqlionnwyzhgd.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhaWRuZGttcWxpb25ud3l6aGdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMDgxNzksImV4cCI6MjA2ODc4NDE3OX0.-cDX4BPxLOq2g2-ejxX1kRMxkU-ACQdDjIaj9y-YANY";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
